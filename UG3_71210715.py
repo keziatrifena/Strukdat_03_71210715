40383cb9a52929d78dba8886c24d9df67c4a8628
Kezia Trifena <93313412+keziatrifena@users.noreply.github.com>
2022-09-12 19:06:16 +0700

    Add files via upload

---
+++ UG3_71210715.py
@@ -0,0 +1,12 @@
+kali = input("Masukkan kalimat: ")
+kata = input("Masukkan kata yang ingin dicari: ")
+
+kali = kali.lower().split()
+kata = kata.lower()
+count = 0
+
+for i in kali:
+    if i == kata:
+        count += 1
+
+print(count)
